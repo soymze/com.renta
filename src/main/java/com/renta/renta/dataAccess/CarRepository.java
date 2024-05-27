5aeb27e00eae62ad026ae9db90d942182ddc9239
Emre <soylmze@gmail.com>
2024-05-27 20:37:02 +0300

    son degisiklikler

--- src/main/java/com/renta/renta/dataAccess/CarRepository.java
+++ src/main/java/com/renta/renta/dataAccess/CarRepository.java
@@ -1,9 +1,17 @@
 package com.renta.renta.dataAccess;
 
 import com.renta.renta.entities.Car;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CarRepository extends JpaRepository<Car, Integer> {
+import java.util.List;
+
+public interface CarRepository extends PagingAndSortingRepository<Car, Integer> {
 
    // void add();
+
+    @Query("SELECT c FROM Car c WHERE NOT EXISTS(SELECT 1 FROM Booking b WHERE b.car=c)")
+    List<Car> list();
 }
---
+++ src/main/java/com/renta/renta/entities/Booking.java
@@ -0,0 +1,26 @@
+package com.renta.renta.entities;
+
+import com.renta.renta.core.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+@EqualsAndHashCode(callSuper = true)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Booking extends BaseEntity<Integer> {
+
+    @ManyToOne
+    @JoinColumn(name = "CAR_ID")
+    private Car car;
+
+    private Double price;
+
+    //rezervasyonid
+}
